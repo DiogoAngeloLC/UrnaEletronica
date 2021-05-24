4660ed1e2346438c2bd6ed5374f13a9afdd3178e
DiogoAngeloLC <83246865+DiogoAngeloLC@users.noreply.github.com>
2021-05-24 18:00:41 -0300

    Add files via upload

---
+++ Urna/etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '12345',
+                nome: 'Bruno Formiga',
+                partido: 'Partido Formigados',
+                fotos:[
+                    {url: 'formiga.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '67890',
+                nome: 'Vitor Sergio Rodrigues',
+                partido: 'Partido VSR',
+                fotos:[
+                    {url:'vsr.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Casimiro Miguel',
+                partido: 'Partido Rede Gordo',
+                vice: 'Luis Felipe Freitas',
+                fotos:[
+                    {url: 'caze.png', legenda: 'Prefeito'},
+                    {url: 'luis.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '20',
+                nome: 'Pedro Certezas',
+                partido: 'Partido Com Certezas',
+                vice: 'Guilherme Beltrao',
+                fotos:[
+                    {url:'certezas.png', legenda: 'Prefeito'},
+                    {url:'beltrao.png', legenda: 'Vice-Prefeito', small: true}
+
+                ]
+            },
+        ]
+    },
+]
